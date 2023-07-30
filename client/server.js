fb747819ad7a53c670c0f3e8c0bb8efa00655dc4
meakashdash <cen.20bcec10@silicon.ac.in>
2023-07-30 20:16:20 +0530

    Console.log changes

--- client/server.js
+++ client/server.js
@@ -34,7 +34,7 @@ app.prepare().then(()=>{
         if(err){
             throw err
         }
-        console.log("Ready on http://localhost:8000")
+        console.log("Ready on http://localhost:3000")
     })
 }).catch(err=>{
     console.log('Error:',err)
